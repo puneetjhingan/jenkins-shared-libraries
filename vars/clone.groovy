7e44a937f8546ae8ce8a33b83cea355c555d5e4a
Puneet Jhingan <puneetjhingan13@gmail.com>
2025-05-12 13:54:55 +0000

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,8 +1,3 @@
-def call() {
-    echo "Checking out repository..."
-    checkout scm
+def call(String url, String branch){
+  git url: "${url}", branch: "${branch}"
 }
-
-// def call(String url, String branch){
-//   git url: "${url}", branch: "${branch}"
-// }
