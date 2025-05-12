08ce8d33d7163a719733ddb65fa7e833c6341ec6
Puneet Jhingan <puneetjhingan13@gmail.com>
2025-05-12 13:51:27 +0000

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,9 +1,8 @@
-#!/usr/bin/env groovy
-
-/**
- * Checkout the repository
- */
 def call() {
     echo "Checking out repository..."
     checkout scm
 }
+
+// def call(String url, String branch){
+//   git url: "${url}", branch: "${branch}"
+// }
